948fcb828c20966b6cfaa996afd5bbfdbb198f4d
NEELAM GAYKWAD <neelg1805@gmail.com>
2025-09-17 22:05:53 +0530

    cc

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -6,7 +6,7 @@ export default function DashboardLayout() {
     <div className="min-h-screen bg-gray-50">
       <DashboardNav />
       <main className="container mx-auto px-4 py-8">
-        <Outlet />
+         <Outlet />
       </main>
     </div>
   );
